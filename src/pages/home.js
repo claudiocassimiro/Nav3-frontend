1167d44cf65f62eaa842f2334988423b2262bc97
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-04-29 17:04:07 -0300

    add modal to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../public/logo.png";
+import { useDialogState } from "reakit/Dialog";
+import SearchModal from "../components/SearchModal";
 
 import styles from "../styles/Home.module.css";
+import FormSearchData from "../components/FormSearchData";
 
 export default function Home() {
+  const modal = useDialogState();
+
+  const handleModal = () => {
+    modal.show();
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.containerLogo}>
           <Image src={logo} alt="Logo" width={50} height={50} />
         </div>
-        <button className={styles.openModalButton}>
+        <button onClick={handleModal} className={styles.openModalButton}>
           <span className={styles.text}>Faça uma busca</span>
         </button>
         <button type="button" className={styles.OptionsButton}>
@@ -23,6 +32,11 @@ export default function Home() {
           <span className={styles.cardRole}>Listar Pessoas</span>
         </div>
       </main>
+      {modal.visible && (
+        <SearchModal modal={modal} windowTitle="Faça sua busca">
+          <FormSearchData />
+        </SearchModal>
+      )}
     </div>
   );
 }
