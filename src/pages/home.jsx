f68999d68fca7327583b93cda6d507657b86bb11
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-04-29 21:35:41 -0300

    nex extension for the file

---
+++ src/pages/home.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import logo from "../public/logo.png";
+import { useDialogState } from "reakit/Dialog";
+import SearchModal from "../components/SearchModal";
+
+import styles from "../styles/Home.module.css";
+import FormSearchData from "../components/FormSearchData";
+
+export default function Home() {
+  const modal = useDialogState();
+  const [peopleData, setPeopleData] = useState({});
+
+  const handleModal = () => {
+    modal.show();
+  };
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.wrapper}>
+          <div className={styles.containerLogo}>
+            <Image src={logo} alt="Logo" width={50} height={50} />
+          </div>
+          <button onClick={handleModal} className={styles.openModalButton}>
+            <span className={styles.text}>Faça sua busca</span>
+          </button>
+          <button type="button" className={styles.OptionsButton}>
+            Preferências
+          </button>
+        </div>
+      </header>
+      <main className={styles.mainContainer}>
+        <div className={styles.optionsCard}>
+          <span className={styles.cardRole}>Listar Pessoas</span>
+        </div>
+      </main>
+      {modal.visible && (
+        <SearchModal modal={modal} windowTitle="Faça sua busca">
+          <>
+            <FormSearchData setPeopleData={setPeopleData} />
+            {peopleData.name && (
+              <div className={styles.peopleInfoCard}>
+                {/* Aqui vou imprimir os dados do usuario encontrado */}
+              </div>
+            )}
+          </>
+        </SearchModal>
+      )}
+    </div>
+  );
+}
